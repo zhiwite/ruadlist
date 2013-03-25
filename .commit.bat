420be0308945f6032f1c695881988f7e6d80a58b
lain.halfbit <lain.halfbit@localhost>
2013-03-25 18:06:45 +0200

    http://serialu.net/svaty/

---
+++ .commit.bat
@@ -0,0 +1,14 @@
+@echo off
+@echo Обновление подписок...
+@echo Расчёт контрольных сумм:
+hash.pl advblock.txt
+@echo * advblock.txt
+hash.pl antinuha.txt
+@echo * antinuha.txt
+hash.pl bitblock.txt
+@echo * bitblock.txt
+hash.pl cntblock.txt
+@echo * cntblock.txt
+@echo Внесение изменений на сервер...
+start thg commit
+start thg sync
